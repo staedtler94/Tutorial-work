515003e3a5f4ee70f0fe3b88a845e4b253113b1f
Siddhesh Dhage <siddheshdhage@yahoo.com>
2024-06-06 16:13:05 +0100

    Structured Tutorials Execution

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 import Services.BinaryChecker;
 import Services.EmailChecker;
+import interfaces.Tutorial;
 import models.EmailCheckerModel;
+import tutorials.ExploreTutorials;
+import tutorials.TutorialList;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -9,42 +12,8 @@ public class Main {
 
         System.out.println("Welcome to Java Samples and Tutorials");
 
-        // RegEx Samples
-        // Email Checker
-        String str1 = "example@example.com"; // false not gmail
-        String str2 = "example@gmail.com"; // true
-
-        String str3 = "example134@gmail.com"; // true
-        String str4 = "example_134@gmail.com"; // false there is _S
-
-        String regEx = "[a-zA-Z0-9]*@gmail.com";
-
-        System.out.println(str1.matches(regEx));
-        System.out.println(str2.matches(regEx));
-
-        System.out.println(str3.matches(regEx));
-        System.out.println(str4.matches(regEx));
-
-        EmailChecker emailChecker = new EmailChecker();
-        EmailCheckerModel model = emailChecker.getEmailAttributes(str1);
-
-        System.out.println(model.getUserName());
-        System.out.println(model.getDomain());
-
-        // Binary Checker
-
-        int b = 11101101;
-        String binaryStringify = b + "";
-        BinaryChecker bChecker = new BinaryChecker();
-        System.out.println(bChecker.isBinary(binaryStringify));
-
-        int notBin = 21093;
-        String notBinString = notBin + "";
-        System.out.println(bChecker.isBinary(notBinString));
-
-        // Hexadecimal Checker
-        int hex = 0xA12;
-        String hexStringify = hex + "";
+        Tutorial trainings = new ExploreTutorials().explore(TutorialList.REG_EX);
+        trainings.executeCode();
 
     }
 }
---
+++ src/tutorials/TutorialList.java
@@ -0,0 +1,7 @@
+package tutorials;
+
+// this is the iterable list of Tutorials we can go through in the future
+public enum TutorialList {
+    REG_EX,
+    INHERITANCE;
+}
